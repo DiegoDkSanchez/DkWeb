035dabe7110c4004d11326a49193f718bcee516b
DiegoDkSanchez <diegodksanchez@gmail.com>
2019-03-14 23:06:03 -0700

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dk-Web';
+  public lottieConfig: Object;
+    private anim: any;
+    private animationSpeed: number = 1;
+ 
+    constructor() {
+        this.lottieConfig = {
+            path: 'assets/welcome.json',
+            renderer: 'canvas',
+            autoplay: true,
+            loop: false
+        };
+    }
+ 
+    handleAnimation(anim: any) {
+        this.anim = anim;
+    }
+
+    
+ 
+    stop() {
+        this.anim.stop();
+    }
+ 
+    play() {
+        this.anim.play();
+    }
+ 
+    pause() {
+        this.anim.pause();
+    }
+ 
+    setSpeed(speed: number) {
+        this.animationSpeed = speed;
+        this.anim.setSpeed(speed);
+    }
 }
